a673d1443db25e537fbcaff99f5afc61b7477986
tcptsai <pytsai@gatech.edu>
2016-03-30 00:41:12 -0400

    PaulMap2

--- ImportImage.py
+++ ImportImage.py
@@ -3,70 +3,60 @@ from PIL import Image
 
 class ImportImage:
 
-	def __init__(self, width = 1650, height = 400):
+	def __init__(self, beach, width, height, nBunker):
+		self.beach = beach
 		self.width = width
 		self.height = height
+		self.nBunker = nBunker
 		self.matrix = [[-1 for x in range(self.height)] for x in range(self.width)]
 
-	def setSea(self):
-		im = Image.open("image/map0.bmp").getdata()
+	def set(self):
+		im = Image.open("image/" + self.beach + ".bmp").getdata()
 		i = 0
 		for y in range(self.height):
 			for x in range(self.width):
-				if im[i] == (0, 0, 0):
+				if im[i] == (0, 128, 255):
 					self.matrix[x][y] = 0
-				i += 1
-	
-	def setBeach(self):
-		im = Image.open("image/map1.bmp").getdata()
-		i = 0
-		for y in range(self.height):
-			for x in range(self.width):
-				if im[i] == (0, 0, 0):
+				elif im[i] == (255, 255, 0):
 					self.matrix[x][y] = 1
-				i += 1
-
-	def setLand(self):
-		im = Image.open("image/map2.bmp").getdata()
-		i = 0
-		for y in range(self.height):
-			for x in range(self.width):
-				if im[i] == (0, 0, 0):
+				elif im[i] == (255, 128, 0):
 					self.matrix[x][y] = 2
-				i += 1
-
-	def setSlope(self):
-		im = Image.open("image/map3.bmp").getdata()
-		i = 0
-		for y in range(self.height):
-			for x in range(self.width):
-				if im[i] == (0, 0, 0):
+				elif im[i] == (128, 128, 0):
 					self.matrix[x][y] = 3
+				else:
+					for t in range(self.nBunker):
+						if im[i] == (255, 0, t):
+							self.matrix[x][y] = 4 + t
 				i += 1
-
-	def setBunker(self, nBunker = 13):
-		for t in range(4, 4 + nBunker):
-			im = Image.open("image/map" + str(t) + ".bmp").getdata()
-			i = 0
-			for y in range(self.height):
-				for x in range(self.width):
-					if im[i] == (0, 0, 0):
-						self.matrix[x][y] = t
-					i += 1
 
 	def writeFile(self):
-		text_file = open("image/map.txt", "w")
+		text_file = open("image/" + self.beach + ".txt", "w")
 		for y in range(self.height):
 			for x in range(self.width):
 				text_file.write(str(self.matrix[x][y]) + " ")
 			text_file.write("\n")
 
+omaha = ImportImage("omaha", 6600, 1600, 13)
+omaha.set()
+omaha.writeFile()
+print("omaha done")
 
+utah = ImportImage("utah", 5600, 2100, 7)
+utah.set()
+utah.writeFile()
+print("utah done")
 
-ii = ImportImage()
-ii.setSea()
-ii.setBeach()
-ii.setLand()
-ii.setSlope()
-ii.setBunker()
-ii.writeFile()
+gold = ImportImage("gold", 3700, 2100, 6)
+gold.set()
+gold.writeFile()
+print("gold done")
+
+juno = ImportImage("juno", 1800, 2400, 3)
+juno.set()
+juno.writeFile()
+print("juno done")
+
+sword = ImportImage("sword", 2500, 2500, 3)
+sword.set()
+sword.writeFile()
+print("sword done")
---
+++ image/readme.txt
@@ -0,0 +1,5 @@
+0: sea
+1: beach
+2: land
+3: slope
+4-: bunker
