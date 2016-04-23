71659d3fa0fce983cf416541f562d99ebf634a5d
Ziran Chen <ziranchen@lawn-128-61-16-125.lawn.gatech.edu>
2016-04-23 11:41:26 -0400

    add constants

---
+++ SimConst.py
@@ -0,0 +1,18 @@
+class SimConst:
+	'''
+	Constants for the simulation
+	'''
+
+	RandomSeed = 100
+
+	Bunker_Default_Shots = 100
+	Bunker_Max_Health = 100
+	Bunker_Health_Decrease_Max = 3
+	Bunker_Health_Decrease_Min = 1
+
+	Soldier_Health_Decrease_Max = 3
+	Soldier_Health_Decrease_Min = 1
+	Soldier_Health_Decrease_At_Bunker_Max = 30
+	Soldier_Health_Decrease_At_Bunker_Min = 30
+
+	Default_Cone_Value = -1
